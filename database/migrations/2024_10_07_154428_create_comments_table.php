7ccb60d0fbab5c70f0911b1849d51e27302211c6
Gustavo Beitum <beitmh@gmail.com>
2024-10-08 10:34:54 -0300

    feat: adicionar atributos na migration comments

--- database/migrations/2024_10_07_154428_create_comments_table.php
+++ database/migrations/2024_10_07_154428_create_comments_table.php
@@ -15,7 +15,14 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
+            $table->string('comment', 255);
             $table->timestamps();
+
+            //foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
         });
     }
 
