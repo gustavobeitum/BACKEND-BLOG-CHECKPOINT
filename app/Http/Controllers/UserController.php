123d23404414394e5a8f55a494fda48fc6c84019
Gustavo Beitum <beitmh@gmail.com>
2024-10-09 13:26:31 -0300

    feat: adicionar funcionalidade no controller user e adiconar novo parametro que faltava em user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,17 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        // Retrieve all users
+        $users = User::all();
+        return response()->json($users);
     }
 
     /**
@@ -34,7 +28,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the incoming request
+        $request->validate([
+            'name' =>['required','string','max:255', 'unique:users'],
+            'email' =>['required','string','email','max:255','unique:users'],
+            'password' => ['string','min:8'],
+            'is_admin' => ['required']
+        ]);
+
+        // Create a new user with the validated data
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password? bcrypt($request->password) : null,
+            'is_admin' => $request->is_admin,
+        ]);
+        return response()->json($user);
     }
 
     /**
@@ -45,18 +54,12 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        // Retrieve the user with the given ID
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['messagem' => 'Usuário não encontrado'], 404);
+        }
+        return response()->json($user);
     }
 
     /**
@@ -67,8 +70,27 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
+    {   
+        $user = User::find($id);
+        $request->validate([
+            'name' =>['string','max:255', Rule::unique('users')->ignore($user->id)],
+            'email' =>['string','email','max:255',Rule::unique('users')->ignore($user->id)],
+            'is_admin' => ['required']
+        ]);
+
+        if ($user ===  null) {
+            return response()->json(['Erro' => 'Impossível realizar a atualização, usuário não encontrado'], 404);
+        }
+        
+        $user ->update([
+            'name' => $request->name ?: $user->name,
+            'email' => $request->email ?: $user->email,
+            'is_admin' => $request->is_admin ?: $user->is_admin
+        ]);
+
+        $user->save();
+        return response()->json($user);
+
     }
 
     /**
@@ -79,6 +101,11 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        if ($user === null) {
+            return response()->json(['Erro' => 'Impossível deletar, usuário não encontrado'], 404);
+        }
+        $user->delete();
+        return response()->json(['messagem' => 'Usuário deletado com sucesso']);
     }
 }
