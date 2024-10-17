99d51d3cae89ae0785bfe84b1530d1303bf9c841
Gustavo Beitum <beitmh@gmail.com>
2024-10-17 12:35:01 -0300

    feat: criar uma middleware para bloqueio de rotas

---
+++ app/Http/Middleware/CheckUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->is_admin == 'normal') {
+            return response()->json(['mensage' => 'usuario não autorizado'], Response::HTTP_NO_CONTENT);
+        }
+        return $next($request);
+    }
+}
