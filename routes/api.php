a7b9536099a05b67bf4094d7f8a731de465db06a
Gustavo Beitum <beitmh@gmail.com>
2024-10-17 12:33:46 -0300

    feat: incluir rotas com middleware e groups

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\SavedPostController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\ParagraphController;
-use App\Http\Controllers\PhotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +28,58 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('/user', UserController::class);
-Route::apiResource('/post', PostController::class);
-Route::apiResource('/comment', CommentController::class);
-Route::apiResource('/answer', AnswerController::class);
-Route::apiResource('/paragraph', ParagraphController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::middleware('check')->group(function () {
+
+        Route::post('/post', [PostController::class, 'store']);
+
+        Route::put('/post/{id}', [PostController::class, 'update']);
+
+        Route::delete('/post/{id}', [PostController::class, 'destroy']);
+
+        Route::post('/paragraph', [ParagraphController::class, 'store']);
+
+        Route::put('/paragraph/{id}', [ParagraphController::class, 'update']);
+
+        Route::delete('/paragraph/{id}', [ParagraphController::class, 'destroy']);
+    });
+
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+    Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store']);
+
+    Route::get('/savedpost', [SavedPostController::class, 'index']);
+    Route::post('/savedpost', [SavedPostController::class, 'store']);
+
+    Route::get('/comment', [CommentController::class, 'index']);
+    Route::get('/comment/{id}', [CommentController::class, 'show']);
+    Route::post('/comment', [CommentController::class, 'store']);
+    Route::put('/comment/{id}', [CommentController::class, 'update']);
+    Route::delete('/comment/{id}', [CommentController::class, 'destroy']);
+
+    Route::get('/answer', [AnswerController::class, 'index']);
+    Route::get('/answer/{id}', [AnswerController::class, 'show']);
+    Route::post('/answer', [AnswerController::class, 'store']);
+    Route::put('/answer/{id}', [AnswerController::class, 'update']);
+    Route::delete('/answer/{id}', [AnswerController::class, 'destroy']);
+});
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::post('/user', [UserController::class, 'store']);
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
+
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/post/{id}', [PostController::class, 'show']);
+
+Route::get('/paragraph', [ParagraphController::class, 'index']);
+Route::get('/paragraph/{id}', [ParagraphController::class, 'show']);
